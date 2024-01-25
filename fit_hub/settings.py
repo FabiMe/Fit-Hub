cf1a754991a927a7f900bf6d8716f052f7f70a6c
Fabian Meister <fabimeister@gmx.de>
2024-01-25 18:44:14 +0000

    remove secret key

--- fit_hub/settings.py
+++ fit_hub/settings.py
@@ -9,10 +9,10 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'p@ei#69*b*zz3u4yie-$()@cy^l(+x9&@6ypx+r0lm(3%_9hr7'
+SECRET_KEY = os.environ.get('SECRET_KEY', '')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = 'DEVELOPMENT' in os.environ
 
 ALLOWED_HOSTS = ['fit-hub-764c345e8c73.herokuapp.com','https://fit-hub-764c345e8c73.herokuapp.com/', '8000-fabime-fithub-mv7f6ra4jy9.ws-eu107.gitpod.io']
 
